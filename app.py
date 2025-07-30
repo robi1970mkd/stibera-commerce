5d3436aac5d88e9b07311f426edfdf71ff928ad3
robi1970mkd <RobiRasPelagon@proton.me>
2025-07-30 15:08:07 +0200

    Add files via upload
    SCZ2: Initial setup for deployment

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Welcome to StiberaCommerce!"
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
