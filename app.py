b8e617bb89522827d67fb86f4b23e764a69f73eb
robi1970mkd <RobiRasPelagon@proton.me>
2025-07-30 12:20:40 +0200

    Add files via upload
    Initial deployment of StiberaCommerce Flask app

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "Welcome to StiberaCommerce!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
