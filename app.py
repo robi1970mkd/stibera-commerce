4538d420c49d431c728528897fdafd5426c207f1
robi1970mkd <RobiRasPelagon@proton.me>
2025-07-30 11:51:01 +0200

    Add files via upload
    Initial upload for StiberaCommerce service – includes main app, requirements, and Git config.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print('StiberaCommerce App Running')
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to StiberaCommerce!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
